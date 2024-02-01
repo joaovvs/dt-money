ef429b82dbedd4a5d18abd78dfc746081e2f7245
Joao Vinicius <joaovvs@gmail.com>
2024-02-01 18:51:34 -0300

    new transaction with modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/themes/default"
 import { GlobalStyles } from "./styles/global"
+import Transactions from "./pages/Transactions"
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <h1>Hello world!</h1>
+      <Transactions/>
+      
     </ThemeProvider>
   )
 }
